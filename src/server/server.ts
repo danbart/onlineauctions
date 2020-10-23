f8f940dbb9e78695da09e9707f579d801bca41f2
Danilo  Solórzano <danilo_solorzano@hotmail.com>
2020-10-23 12:57:31 -0600

    update server socket

--- src/server/server.ts
+++ src/server/server.ts
@@ -10,7 +10,7 @@ export default class Server {
   public app: expres.Application;
   public port: number;
 
-  public io: socketIO.Server;
+  // public io: socketIO.Server;
   private httpServer: http.Server;
 
   private constructor() {
@@ -18,37 +18,37 @@ export default class Server {
     this.port = SERVER_PORT;
 
     this.httpServer = new http.Server(this.app);
-    this.io = socketIO(this.httpServer);
+    // this.io = socketIO(this.httpServer);
 
-    this.escucharSockets();
+    // this.escucharSockets();
   }
 
   public static get instance() {
     return this._instance || (this._instance = new this());
   }
 
-  private escucharSockets() {
-    console.log("Escuchando Conexiones - sockets");
+  // private escucharSockets() {
+  //   console.log("Escuchando Conexiones - sockets");
 
-    this.io.on("connection", (cliente) => {
-      // console.log('Cliente Conectado');
+  //   this.io.on("connection", (cliente) => {
+  //     // console.log('Cliente Conectado');
 
-      // conectar cliente
-      socket.conectarCliente(cliente, this.io);
+  //     // conectar cliente
+  //     socket.conectarCliente(cliente, this.io);
 
-      //Configurar usuario
-      socket.configurarUsuario(cliente, this.io);
+  //     //Configurar usuario
+  //     socket.configurarUsuario(cliente, this.io);
 
-      // Mensajes
-      socket.mensaje(cliente, this.io);
+  //     // Mensajes
+  //     socket.mensaje(cliente, this.io);
 
-      // Desconectar
-      socket.desconectar(cliente, this.io);
+  //     // Desconectar
+  //     socket.desconectar(cliente, this.io);
 
-      // Obtener usuarios
-      socket.obtenerUsuarios(cliente, this.io);
-    });
-  }
+  //     // Obtener usuarios
+  //     socket.obtenerUsuarios(cliente, this.io);
+  //   });
+  // }
 
   start(callback: Function) {
     this.httpServer.listen(this.port, callback());
