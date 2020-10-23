9d0c869897910d1a31089996aebcc451322f9988
Danilo  Solórzano <danilo_solorzano@hotmail.com>
2020-10-23 12:59:16 -0600

    update class user

--- src/classes/user.ts
+++ src/classes/user.ts
@@ -8,71 +8,28 @@ export class User {
 
   constructor() {}
 
-  public agregar = (usuario: ModelUser) => {
-    const query = `
-        INSERT INTO test_user (user_name, user_surname, user_email, 
-            user_password) values ('${usuario.name}', '${usuario.last_name}', 
-            '${usuario.email}', '${usuario.password}' ) `;
-
-    return query;
-  };
-
-  public actualizarNombre(id: number, nombre: string) {
-    // en esta parte se aria la consulta a la base de datos
-    // para ver si existe el usuario
-    for (let usuario of this.lista) {
-      if (usuario.id_user === id) {
-        usuario.name = nombre;
-        break;
-      }
-    }
-
-    console.log("============= Actualizando usuario ============");
-    console.log(this.lista);
-  }
-
-  // Obtener lista de usuarios y usuario dependiendo el caso
-  public getLista = (user?: number) => {
-    const query = `
-        SELECT user_id, user_name, user_surname,user_role,user_estado,
-        user_avatar, user_email, user_createdAt, user_updatedAt, 
-        user_deletedAt FROM test_user`;
-
-    if (user) return query + ` where user_id=${user}`;
-
-    return query;
-  };
-
-  // Obtener usuario en una sala en particular
-  public getUsuarioEnSala(sala: string) {
-    // return this.lista.filter ( usuario => usuario.sala === sala );
-  }
-
-  // Borrar un usuario
-  public borrarUsuario(id: string) {
-    // const tempUsuario = this.getUsuario( id );
-    // this.lista = this.lista.filter( usuario => usuario.id !== id );
-    // return tempUsuario;
-  }
-
+ 
   getUsers = async (req: Request, res: Response) => {
-    const query = `
-      SELECT * FROM user
-    `;
     const result: IResponse = {
       ok: false,
     };
 
-    await MySql.executeQuery(query, (err: any, users: ModelUser[]) => {
-      if (err) {
-        result.ok = false;
-        result.error = err;
-      } else {
-        result.ok = true;
-        result.data = users;
-      }
-    });
-
-    res.json(result);
+    try {      
+      await MySql.executeQuery(
+        `SELECT id_user, name,last_name,email,address,phone,active,avatar,web_site,facebook,twitter,remember_token, created_at, updated_at FROM user`
+      )
+        .then((data: any) => {
+          result.ok = true;
+          result.data = data;
+        })
+        .catch((err) => {
+          result.ok = false;
+          result.error = err;
+        });
+  
+      res.json(result);
+    } catch (error) {
+      console.log(error);
+    }
   };
 }
