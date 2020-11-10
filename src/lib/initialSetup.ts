39a538b977997be600ff89b30e3f84df5315b505
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2020-11-09 18:50:58 -0600

    update initials errors

--- src/lib/initialSetup.ts
+++ src/lib/initialSetup.ts
@@ -1,4 +1,6 @@
 import bcrypt from "bcrypt";
+import moment from "moment";
+import { FORMAT_DATE_TIME } from "../global/environment";
 import { ModelRole } from "../models/role";
 import { ModelUser } from "../models/user";
 import MySql from "../mysql/mysql";
@@ -33,10 +35,10 @@ export const createAdmin = async () => {
 
   if (users.length === 0) {
     const admin = new ModelUser();
-    admin.email = "admin@localhost";
+    admin.email = "admin@subastasonline.me";
     admin.name = "admin";
-    admin.active = new Date().toJSON().slice(0, 19).replace("T", " ");
-    admin.password = await bcrypt.hash("admin", 10);
+    admin.active = moment().format(FORMAT_DATE_TIME);
+    admin.password = await bcrypt.hash("admin123", 10);
 
     await MySql.executeQuery(
       `INSERT INTO user(name, email,active, password) VALUES ("${admin.name}","${admin.email}", "${admin.active}","${admin.password}" );`
