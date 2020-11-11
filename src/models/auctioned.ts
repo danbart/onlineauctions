6522eb452168c2e8482f3442c1a3813826193667
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2020-11-11 12:50:01 -0600

    change string to number

--- src/models/auctioned.ts
+++ src/models/auctioned.ts
@@ -1,7 +1,7 @@
 export class ModelAuctioned {
   id_auctioned: number;
   id_auction: number;
-  amount: string;
+  amount: number;
   id_user: number;
   description: string;
   cancelled: string;
@@ -12,7 +12,7 @@ export class ModelAuctioned {
     this.id_auctioned = 0;
     this.id_auction = 0;
     this.id_user = 0;
-    this.amount = "";
+    this.amount = 0;
     this.description = "";
     this.cancelled = "";
     this.created_at = "";
